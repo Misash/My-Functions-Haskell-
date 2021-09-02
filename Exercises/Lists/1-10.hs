4dcf52c9a400bdd43d2625aa99b915c8dc2bea10
Misash <misash@outlook.com>
2021-09-01 22:44:19 -0500

    solved P7

--- Exercises/Lists/1-10.hs
+++ Exercises/Lists/1-10.hs
@@ -87,4 +87,18 @@ isPalindrome list =
 
 data NestedList a = Elem a | List [NestedList a]
 
+flatten :: NestedList a -> [a]
+flatten (List []) = []
+flatten (Elem a) = [a]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
+
+
+
+
+
+
+
+
+
 
