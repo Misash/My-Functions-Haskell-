0f8323682a7b1dfcf81d36057614b1f8f662dd0a
Misash <misash@outlook.com>
2021-09-14 18:07:00 -0500

    Rna solved

---
+++ Exercism/Rna.hs
@@ -0,0 +1,20 @@
+
+
+
+
+toRNA :: String -> Either Char String
+toRNA xs = if length txt == 0 then a else b 
+    where txt = [x | x <- xs , x /= 'G' && x /= 'C' && x /= 'T' &&  x/= 'A']
+          a = Right [rna | x <- xs , let rna = case x of 'G' -> 'C'
+                                                         'C' -> 'G'
+                                                         'T' -> 'A'
+                                                         'A' -> 'U']
+          b = Left (head txt)
+       
+
+
+
+
+
+
+
